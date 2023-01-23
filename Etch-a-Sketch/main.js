bc9ae443023eed34f0fbdc40281a988f10c65735
Donald Doricent <ddoricent97@gmail.com>
2023-01-23 07:47:55 -0500

    Added random color button functionality along with reset button.

--- Etch-a-Sketch/main.js
+++ Etch-a-Sketch/main.js
@@ -1,3 +1,6 @@
+let color = 'black';
+let click = true;
+
 function populateBoard(size) {
     let board = document.querySelector('.board');
     let squares = board.querySelectorAll('div');
@@ -24,5 +27,25 @@ function changeSize(input) {
 }
 }
 function colorSquare() {
-    this.style.backgroundColor = "black";
+    if(click) {
+        if(color === 'random'){
+            this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+        } else {
+            this.style.backgroundColor = color;
+        }
+    }
+} 
+
+function changeColor(choice) {
+    color = choice;
 }
+
+function resetBoard(){
+    let board = document.querySelector('.board');
+    let squares = board.querySelectorAll('div');
+    squares.forEach((div) => div.style.backgroundColor = 'white');
+}
+
+document.querySelector('body').addEventListener('click' , () => {
+    click = !click;
+});
